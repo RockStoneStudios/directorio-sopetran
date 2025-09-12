5cbd55d5a52cb41ed1b0062454230b070f5609b9
OMAR <rockstonestudios666@gmail.com>
2025-09-12 13:52:02 -0500

    logos top-navbar

--- components/buttons/add-business-buttons.tsx
+++ components/buttons/add-business-buttons.tsx
@@ -13,12 +13,12 @@ const AddBusinessButton = () => {
        router.push("/business/add");
     }
   return (
-      <MenubarMenu>
+      <MenubarMenu >
             <MenubarTrigger className='text-base font-normal'>
                                  
-                <span onClick={handleClick} className='flex items-center cursor-pointer' >
-                <Plus size={16} />
-                <span className='text-sm'>Agregar</span>
+                <span onClick={handleClick} className='flex items-center cursor-pointer mr-2' >
+                <Plus size={16} className='mr-1'/>
+                <span className='text-sm'>Agregar </span>
                 </span>
                                  
           </MenubarTrigger>
